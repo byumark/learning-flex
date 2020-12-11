1ff69718447e361ea6d0ff3232f07262575bf54a
Mark Hatch <mark-hatch@pluralsight.com>
2020-12-11 15:30:27 -0700

    first pass

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,200 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+
+const BoxItem = ({ text, width = '50px', height = '50px' }) => (
+  <div
+    style={{
+      width,
+      height,
+      backgroundColor: 'red',
+      margin: '3px',
+    }}
+  >
+    {text}
+  </div>
+)
+
+const FlexContainer = ({
+  width,
+  height,
+  flexDirection,
+  flexWrap,
+  justifyContent,
+  alignItems,
+  children,
+}) => (
+  <div
+    style={{
+      display: 'flex',
+      backgroundColor: 'lightgray',
+      width,
+      height,
+      flexDirection,
+      flexWrap,
+      justifyContent,
+      alignItems,
+    }}
+  >
+    {children}
+  </div>
+)
+
+const FlexDirection = () => (
+  <div>
+    <h4>flex-direction</h4>
+    <p>row</p>
+    <FlexContainer>
+      <BoxItem text="1" />
+      <BoxItem text="2" />
+      <BoxItem text="3" />
+    </FlexContainer>
+    <p>row-reverse</p>
+    <FlexContainer flexDirection="row-reverse">
+      <BoxItem text="1" />
+      <BoxItem text="2" />
+      <BoxItem text="3" />
+    </FlexContainer>
+    <p>column</p>
+    <FlexContainer flexDirection="column">
+      <BoxItem text="1" />
+      <BoxItem text="2" />
+      <BoxItem text="3" />
+    </FlexContainer>
+    <p>column-reverse</p>
+    <FlexContainer flexDirection="column-reverse">
+      <BoxItem text="1" />
+      <BoxItem text="2" />
+      <BoxItem text="3" />
+    </FlexContainer>
+  </div>
+)
+
+const FlexWrap = () => (
+  <div>
+    <h4>flex-wrap</h4>
+    <p>nowrap</p>
+    <FlexContainer>
+      <BoxItem text="1 width=250px" width="250px" />
+      <BoxItem text="2 width=250px" width="250px" />
+      <BoxItem text="3 width=250px" width="250px" />
+      <BoxItem text="4 width=250px" width="250px" />
+      <BoxItem text="5 width=250px" width="250px" />
+    </FlexContainer>
+    <p>wrap</p>
+    <FlexContainer flexWrap="wrap">
+      <BoxItem text="1 width=250px" width="250px" />
+      <BoxItem text="2 width=250px" width="250px" />
+      <BoxItem text="3 width=250px" width="250px" />
+      <BoxItem text="4 width=250px" width="250px" />
+      <BoxItem text="5 width=250px" width="250px" />
+    </FlexContainer>
+    <p>wrap-reverse</p>
+    <FlexContainer flexWrap="wrap-reverse">
+      <BoxItem text="1 width=250px" width="250px" />
+      <BoxItem text="2 width=250px" width="250px" />
+      <BoxItem text="3 width=250px" width="250px" />
+      <BoxItem text="4 width=250px" width="250px" />
+      <BoxItem text="5 width=250px" width="250px" />
+    </FlexContainer>
+  </div>
+)
+
+const JustifyContent = () => (
+  <div>
+    <h4>justify-content</h4>
+    <p>flex-start</p>
+    <FlexContainer>
+      <BoxItem width="100px" />
+      <BoxItem width="50px" />
+      <BoxItem width="100px" />
+    </FlexContainer>
+    <p>flex-end</p>
+    <FlexContainer justifyContent="flex-end">
+      <BoxItem width="100px" />
+      <BoxItem width="50px" />
+      <BoxItem width="100px" />
+    </FlexContainer>
+    <p>center</p>
+    <FlexContainer justifyContent="center">
+      <BoxItem width="100px" />
+      <BoxItem width="50px" />
+      <BoxItem width="100px" />
+    </FlexContainer>
+    <p>space-between</p>
+    <FlexContainer justifyContent="space-between">
+      <BoxItem width="100px" />
+      <BoxItem width="50px" />
+      <BoxItem width="100px" />
+    </FlexContainer>
+    <p>space-around</p>
+    <FlexContainer justifyContent="space-around">
+      <BoxItem width="100px" />
+      <BoxItem width="50px" />
+      <BoxItem width="100px" />
+    </FlexContainer>
+    <p>space-evenly</p>
+    <FlexContainer justifyContent="space-evenly">
+      <BoxItem width="100px" />
+      <BoxItem width="50px" />
+      <BoxItem width="100px" />
+    </FlexContainer>
+  </div>
+)
+
+const AlignItems = () => (
+  <div>
+    <h4>align-items</h4>
+    <p>stretch</p>
+    <FlexContainer height="300px" alignItems="stretch">
+      <BoxItem width="250px" />
+      <BoxItem width="250px" />
+      <BoxItem width="250px" />
+      <BoxItem width="250px" />
+    </FlexContainer>
+    <p>flex-start</p>
+    <FlexContainer height="300px" alignItems="flex-start">
+      <BoxItem width="250px" height="150px" />
+      <BoxItem width="250px" height="200px" />
+      <BoxItem width="250px" height="100px" />
+      <BoxItem width="250px" height="150px" />
+    </FlexContainer>
+    <p>flex-end</p>
+    <FlexContainer height="300px" alignItems="flex-end">
+      <BoxItem width="250px" height="150px" />
+      <BoxItem width="250px" height="200px" />
+      <BoxItem width="250px" height="100px" />
+      <BoxItem width="250px" height="150px" />
+    </FlexContainer>
+    <p>center</p>
+    <FlexContainer height="300px" alignItems="center">
+      <BoxItem width="250px" height="150px" />
+      <BoxItem width="250px" height="200px" />
+      <BoxItem width="250px" height="100px" />
+      <BoxItem width="250px" height="150px" />
+    </FlexContainer>
+    <p>baseline</p>
+    <FlexContainer height="300px" alignItems="baseline">
+      <BoxItem width="250px" height="150px" />
+      <BoxItem width="250px" height="200px" />
+      <BoxItem width="250px" height="100px" />
+      <BoxItem width="250px" height="150px" />
+    </FlexContainer>
+  </div>
+)
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Learning Flex</title>
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      <h2>Properties for the Parent (flex container)</h2>
+      <FlexDirection />
+      <br />
+      <FlexWrap />
+      <br />
+      <JustifyContent />
+      <br />
+      <AlignItems />
     </div>
   )
 }
